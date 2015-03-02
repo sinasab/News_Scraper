60a0cbfa1ec8bf26ec19462f4f36b0bc46e4779d
Sina Sabet <sinasabet81@gmail.com>
2015-03-01 22:14:30 -0500

    Update outro.rb

--- outro.rb
+++ outro.rb
@@ -1,6 +1,7 @@
 require 'launchy'
 =begin
 	Prints outro to console. Also gives user option to open the file automatically in their default web browser.
+	Contributors: Sina
 =end
 def outro()
 	puts "\n\nEnter a \"1\" and then press enter to have the script automatically open the file in your default web browser. Enter anything else to end the program.\n\nNote: opening the webpage automatically is an experimental feature, and may cause the program to break. You can always manually open the webpage.\n"
